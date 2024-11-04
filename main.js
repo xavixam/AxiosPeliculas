76ca15c7a596b08b3687889f5ffa73911156b0dd
xavi <antunez.xavi.grao@gmail.com>
2024-11-04 11:35:09 +0100

    no message

--- main.js
+++ main.js
@@ -3,21 +3,19 @@ const btn = document.querySelector("#btn")
 
 btn.addEventListener("click", (e) => {
     e.preventDefault()
-    const userName = inputName.value
 
-    async function getPosts(user) {
+    const filmName = inputName.value
+
+    async function getPosts(filmName) {
         try {
-            const api = await axios.get(`https://api.github.com/users/${user}`);
-            const userData = api.data
-            console.log(userData.name);
-            console.log(userData.public_repos);
-            console.log(userData.avatar_url);
+            const a = await axios.get(`https://api.themoviedb.org/3/movie/${filmName}`);
+            console.log(a);
         } catch (error) {
           console.error(error);
         }
     }
 
-    getPosts(userName)
+    getPosts(filmName)
       
     
 })
